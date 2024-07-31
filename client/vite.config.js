d405f8c03f8eabab52bc9d99b189d12fb99c4b04
Booker <bookerngoon@gmail.com>
2024-07-31 13:21:02 -0400

    configured yml and vite.config file to allow access to port 5173

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // This binds the server to all network interfaces
+    port: 5173, // Ensure the port is correct
+    proxy: {
+      '/api': {
+        target: 'http://backend:8000',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
